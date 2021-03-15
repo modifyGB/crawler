2002e571c9558609684c9d01a7f1963c2e17b45a
RHT <2782299413@qq.com>
2021-03-15 17:54:37 +0800

    admin

--- baogao.py
+++ baogao.py
@@ -8,7 +8,6 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
-from email.header import Header
 
 time_now = time.strftime("%Y-%m-%d",time.localtime(time.time()))
 time_yesterday = time.strftime("%Y-%m-%d",time.localtime(time.time()-86400))
@@ -23,8 +22,24 @@ file = '{}.xls'.format(time_yesterday) #文件路径
 sender = '2782299413@qq.com' #发送方邮箱
 passwd = 'gbslaicivtcvdgab' #授权码
 #收件人邮箱
-receivers = ['gdufs_shuwa@163.com','657742829@qq.com','jiangshengyi@163.com','331423904@qq.com']
-
+receivers = [
+    'gdufs_shuwa@163.com',
+    '657742829@qq.com',
+    'jiangshengyi@163.com',
+    '331423904@qq.com',
+    '734200940@qq.com',
+    '2397812053@qq.com',
+    'chenxuanqi6@163.com',
+    '1317787446@qq.com',
+    '912100484@qq.com',
+    '2656141474@qq.com',
+    '1626321547@qq.com',
+    '1044880463@qq.com',
+    '1024463088@qq.com',
+    '2905939881@qq.com',
+    '1031482298@qq.com',
+    '1197072098@qq.com'
+]
 
 def select(execute):
     db = pymysql.connect(sql['host'],sql['user'],sql['password'],sql['db'])
@@ -38,9 +53,9 @@ def excel(path):
     book = xlwt.Workbook(encoding='utf-8',style_compression=0)
 
     xlr = book.add_sheet('每日',cell_overwrite_ok=True)
-    data = select("select website_id,url,website.c_name,website.e_name,count(news.id),`language`.c_name,country.`name` from ((news left join website on news.website_id = website.id) left join `language` on website.lan_id = `language`.id) left join country on website.country_id = country.id where news.cole_time >= '{}' and news.cole_time < '{}' group by website_id".format(time_yesterday,time_now))
+    data = select("select website_id,url,website.c_name,website.e_name,count(news.id),developer,`language`.c_name,country.`name` from ((news left join website on news.website_id = website.id) left join `language` on website.lan_id = `language`.id) left join country on website.country_id = country.id where news.cole_time >= '{}' and news.cole_time < '{}' group by website_id".format(time_yesterday,time_now))
     i = 0
-    for x in ['website_id','url','c_name','e_name','num','lan_id','country_id']:
+    for x in ['website_id','url','c_name','e_name','num','developer','language','country']:
         xlr.write(0,i,x)
         i+=1
     i = 1
@@ -50,13 +65,13 @@ def excel(path):
             xlr.write(i,j,x)
             j+=1
         i+=1
-    xlr.write(i+2,0,'总条数')
-    xlr.write(i+3,0,select("select count(id) from news where news.cole_time >= '{}' and news.cole_time < '{}'".format(time_yesterday,time_now))[0][0])
+    xlr.write(i+1,0,'总条数')
+    xlr.write(i+2,0,select("select count(id) from news where news.cole_time >= '{}' and news.cole_time < '{}'".format(time_yesterday,time_now))[0][0])
 
     xlr = book.add_sheet('累计',cell_overwrite_ok=True)
-    data = select("select website_id,url,website.c_name,website.e_name,count(news.id),`language`.c_name,country.`name` from ((news left join website on news.website_id = website.id) left join `language` on website.lan_id = `language`.id) left join country on website.country_id = country.id group by website_id")
+    data = select("select website_id,url,website.c_name,website.e_name,count(news.id),developer,`language`.c_name,country.`name` from ((news left join website on news.website_id = website.id) left join `language` on website.lan_id = `language`.id) left join country on website.country_id = country.id group by website_id")
     i = 0
-    for x in ['website_id','url','c_name','e_name','num','lan_id','country_id']:
+    for x in ['website_id','url','c_name','e_name','num','developer','language','country']:
         xlr.write(0,i,x)
         i+=1
     i = 1
@@ -66,8 +81,8 @@ def excel(path):
             xlr.write(i,j,x)
             j+=1
         i+=1
-    xlr.write(i+2,0,'总条数')
-    xlr.write(i+3,0,select('select count(id) from news')[0][0])
+    xlr.write(i+1,0,'总条数')
+    xlr.write(i+2,0,select('select count(id) from news')[0][0])
 
     book.save(path)
 
